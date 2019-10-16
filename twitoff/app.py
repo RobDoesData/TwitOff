0da78560b3af66c26aa0468d63c261c3f5eddcc6
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-10-16 11:50:45 -0400

    Cleaned the add user script to update and add, cleaned up HTML formatting.

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,16 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User, Tweet
-from .twitter import add_user
+from .twitter import add_or_update_user
 
 def create_app():
     """Create and configure an instance of the Flask Application"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['ENV'] = config('ENV')
+    app.config['DEBUG'] = config('FLASK_DEBUG')
     DB.init_app(app)
-
+    
     @app.route('/')
     def root():
         users = User.query.all()
@@ -27,9 +28,16 @@ def create_app():
     
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>/', methods=['GET'])
-    def user(name=None):
-        name = name or request.values['user_name']
-        tweets = User.query.filter(User.name == name).one().tweets
-        return render_template('user.html', name=name, tweets=tweets)
-
+    def user(name=None, message=''):
+        name = (name or request.values['user_name'])
+        try:
+            if request.method =='POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name,e)
+            tweets=[]
+            pass
+        return render_template('user.html', name=name, tweets=tweets, message=message)
     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -23,3 +23,33 @@ def add_user(user):
     DB.session.add(db_user)
     DB.session.commit()
     return("Added the user: "+ str(user))
+
+def add_or_update_user(username):
+    """Add or Update a user *and* their Tweets, error if no/private user."""
+    try:
+        #get user info from tweepy API
+        twitter_user = TWITTER.get_user(username)
+        #do a DB query to see if the twitter user already exists
+        db_user = (User.query.get(twitter_user.id) or User(id=twitter_user.id, name=username))
+        DB.session.add(db_user)
+        #we want as many recent non-retweet/reply statuses as we can get
+        #since_id will pull in the most recent tweet ID we have on file
+        #this allows us to only pull in new tweets.
+        tweets = twitter_user.timeline(
+            count=200, exclude_replies=True, include_rts=False, tweet_mode='extended',
+            since_id=db_user.newest_tweet_id)
+        #if there are new tweets to pull in, update them here!
+        #this is skipped if there are no new tweets
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            #Get embedding for tweet and store in DB
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username,e))
+        raise e
+    else:
+        DB.session.commit()
